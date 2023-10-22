3b06bf191dd4c36f3300e8527cda0f21eddc59c4
Jocasmen94 <97995155+Jocasmen94@users.noreply.github.com>
2023-10-22 20:43:55 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,8 +7,3 @@ variable "resource-group-name" {
   default = "resourcegroup"
   description = "The Resource Group name to be deployed"
 }
-
-variable "storage_account_name" {
-  default = "mystorageaccount"
-  description = "The Azure Storage account name."
-}
