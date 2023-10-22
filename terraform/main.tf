c1fd398accc06c461648033bc5f5a1f44c134c2b
Jocasmen94 <97995155+Jocasmen94@users.noreply.github.com>
2023-10-22 20:44:15 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,10 +13,3 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-resource "azurerm_storage_account" "mystorageaccount" {
-  name                = var.storage_account_name
-  resource_group_name = var.resource_group_name
-  location            = "westeurope"
-  sku                 = "Standard_LRS"
-  kind                = "StorageV2"
-}
